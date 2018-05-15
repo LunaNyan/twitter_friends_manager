7f7802639781b9e86f9466ed6ecf2ccceb1692f5
LunaNyan <chkong1998@gmail.com>
2018-05-15 16:11:12 +0900

    some fix (don't worry, previous consumer key was purged ;D)

--- basic.py
+++ basic.py
@@ -10,34 +10,31 @@ import tweepy
 
 Config = configparser.ConfigParser()
 Config
-Config.read("access_key.txt")
+Config.read("settings.txt")
 
-aKey = Config['access_key']['access_token_key']
-aSecret = Config['access_key']['access_token_secret']
+cKey = Config['auth']['consumer_key']
+cSecret = Config['auth']['consumer_secret']
+aKey = Config['auth']['token_key']
+aSecret = Config['auth']['token_secret']
 
-consumerKey = 'SqDP4fVta35RqLfHENK7lGCs7'
-consumerSecret = 'UNDofhfpfIkgRMyGfsE0jMowWf3yasdi71TNxABSqDEnJ0u1Aj'
-
-#auth.OAuthHandler 객체 반환
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 
+consumerKey = cKey
+consumerSecret = cSecret
 accessToken = aKey
-accessTokenSecret = aSecret 
+accessTokenSecret = aSecret
 
-#auth.OAuthHandler r객체에 엑세스토큰 지정
 auth.set_access_token(accessToken, accessTokenSecret)
-
-#API 클래스의 인스턴스 반환 - 읽기, 트윗, 리트윗, DM
 api = tweepy.API(auth)
 
-#트윗포스트
-tweet = str(datetime.datetime.now()) + " 테스트 포스트하기 "
+#tweet post test
+tweet = str(datetime.datetime.now()) + " test post :)"
 api.update_status(status=tweet)
 
 print("Success")
 print()
 
-#읽기
+#read timeline
 tweets = api.home_timeline()
 for tweet in tweets:
     print(tweet.text)
